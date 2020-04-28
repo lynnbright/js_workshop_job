0408f288f1f5856c391ed6b7fd08f745632639f9
lynnchang <tv725987@gmail.com>
2020-04-28 17:19:20 +0800

    WIP 嘗試在搜尋前將原搜尋結果清空

--- feature2.js
+++ feature2.js
@@ -1,14 +1,22 @@
-//目標一：取得一個新的 url
-//步驟
-//1. 取得欄位的值
-//2. 將欄位的值放入 ${} 中
-
 let submit = document.getElementById('submit')
+let nextButton = document.querySelector('.pagination-next')
 
  
-submit.addEventListener('click',function(e){
+submit.addEventListener('click', function(e){
   e.preventDefault();
-  
+  getResult()  
+})
+
+
+nextButton.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('hi')
+})
+
+
+
+
+function getResult(){
   // 取得網址與 json
   let form = document.forms[0]
   let description = form.elements["description"].value
@@ -20,24 +28,27 @@ submit.addEventListener('click',function(e){
   let el1 = searchParams.append("description", description)
   let el2 = searchParams.set("location", place)
   // let el3 = searchParams.set("full_time", fullTime)
-
+  console.log(searchParams.toString())
   let url = `https://still-spire-37210.herokuapp.com/positions.json?${searchParams.toString()}`
+
   console.log(url)
-  
 
   // 將取得的資料印在畫面上
   axios.get(url)
      .then(function(resp){
       let searchResults = resp.data
-       
-      searchResults.map(function(result){
+      
+      
+      let showResults = searchResults.map(function(result){
         let resultUrl = result.url
         let resultTitle = result.title
         let resultLocation = result.location
+
         
         let tr = document.createElement('tr')
         let parent = document.getElementById('job-pannel')
         let fragement = document.createDocumentFragment();
+  
         tr.innerHTML = `<tr>
         <td>
           <h4><a href="${resultUrl}">${resultTitle}</a></h4>
@@ -51,15 +62,10 @@ submit.addEventListener('click',function(e){
           <span class="location">${resultLocation}</span>
         </td>
         </tr>`
-
         fragement.appendChild(tr);
         parent.appendChild(fragement);
-
+        
       })
      })
-     
-})
-
-
-
+}
 
